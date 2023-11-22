7fc3db8f1844ca1933d34dfdd3fa70434859f603
roshinikt <roshinitallam307@gmail.com>
2023-11-22 10:31:22 +0530

    " working "

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,25 @@
+log_file = /temp/expense.file
+
 echo -e "\e[31 install ngnix software \e[0m"
-dnf install nginx -y   &>>/temp/expense.file
+dnf install nginx -y   &>>log_file
 
 echo -e "\e[31 configure ngnix software \e[0m"
 
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/temp/expense.file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
 
 echo -e "\e[31 remove old ngnix software \e[0m"
 
-rm -rf /usr/share/nginx/html/* &>>/temp/expense.file
+rm -rf /usr/share/nginx/html/* &>>log_file
 
  echo -e "\e[31 download frontend application \e[0m"
-curl -o /tmp/frontend.zip ht//expense-artifacts.s3.amazonaws.com/frontend.zip &>>/temp/expense.file
+curl -o /tmp/frontend.zip ht//expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 
 echo -e "\e[31 extract frontend application \e[0m"
 
-cd  /usr/share/nginx/html &>>/temp/expense.file
-unzip /tmp/frontend.zip &>>/temp/expense.file
+cd  /usr/share/nginx/html &>>log_file
+unzip /tmp/frontend.zip &>>log_file
 
 echo -e "\e[31 extract start nginx software \e[0m"
 
-systemctl enable nginx &>>/temp/expense.file
-systemctl restart nginx &>>/temp/expense.file
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
