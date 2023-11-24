81bd4035d5562603c9d563ab065055bdce037f72
roshinikt <roshinitallam307@gmail.com>
2023-11-24 11:36:51 +0530

    shell topics to print in colours

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,24 @@
-echo -e "\e[31 install ngnix software \e[0m"
-dnf install nginx -y
-echo $?
 
-echo -e "\e[31 configure ngnix software \e[0m"
+echo -e "\e[36m install ngnix software \e[0m"
+dnf install nginx -y &>>/temp/expense.log
 
-cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo -e "\e[31 remove old ngnix software \e[0m"
+echo -e "\e[36m configure ngnix software \e[0m"
 
-rm -rf /usr/share/nginx/html/*
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/temp/expense.log
 
- echo -e "\e[31 download frontend application \e[0m"
-curl -o /tmp/frontend.zip ht//expense-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[36m remove old ngnix software \e[0m"
 
-echo -e "\e[31 extract frontend application \e[0m"
+rm -rf /usr/share/nginx/html/* &>>/temp/expense.log
 
-cd  /usr/share/nginx/html
-unzip /tmp/frontend.zip
-echo -e "\e[31 extract start nginx software \e[0m"
+echo -e "\e[36m download frontend application \e[0m"
+curl -o /tmp/frontend.zip ht//expense-artifacts.s3.amazonaws.com/frontend.zip &>>/temp/expense.log
 
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[36m extract frontend application \e[0m"
+
+cd  /usr/share/nginx/html &>>/temp/expense.log
+unzip /tmp/frontend.zip &>>/temp/expense.log
+
+echo -e "\e[36m  starting nginx software \e[0m"
+systemctl enable nginx &>>/temp/expense.log
+systemctl restart nginx &>>/temp/expense.log
