aeda2968cdff4e7fa58ae065bd2f8aafa8600853
roshinikt <roshinitallam307@gmail.com>
2023-11-24 10:32:35 +0530

    " working "

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,28 @@
 echo -e "\e[31 install ngnix software \e[0m"
 dnf install nginx -y
+echo $?
 
 echo -e "\e[31 configure ngnix software \e[0m"
 
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
 
 echo -e "\e[31 remove old ngnix software \e[0m"
 
 rm -rf /usr/share/nginx/html/*
+echo $?
 
  echo -e "\e[31 download frontend application \e[0m"
 curl -o /tmp/frontend.zip ht//expense-artifacts.s3.amazonaws.com/frontend.zip
+echo $?
 
 echo -e "\e[31 extract frontend application \e[0m"
 
 cd  /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo $?
 echo -e "\e[31 extract start nginx software \e[0m"
 
 systemctl enable nginx
 systemctl restart nginx
+echo $?
