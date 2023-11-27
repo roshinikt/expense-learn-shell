975a2aaf28ae5ddbb116f20e0ba5a83c8fbb2414
roshinikt <roshinitallam307@gmail.com>
2023-11-27 11:22:36 +0530

    simple if condition return

--- backend.sh
+++ backend.sh
@@ -1,8 +1,8 @@
-backend_file=/temp/backend.log
+log_file=/temp/expense.log
 color="\e[36m"
 
 echo -e "${color} diable  nodejs software \e[0m"
- dnf module disable nodejs -y &>>backend_file
+ dnf module disable nodejs -y &>>log_file
  if [ $? -eq 0 ]; then
    echo success
  else
@@ -11,55 +11,55 @@ echo -e "${color} diable  nodejs software \e[0m"
 
 
 echo -e "${color} enable  nodejs software \e[0m"
-dnf module enable nodejs:18 -y &>>backend_file
+dnf module enable nodejs:18 -y &>>log_file
 echo $?
 
 echo -e "${color} install nodejs software \e[0m"
-dnf install nodejs -y &>>backend_file
+dnf install nodejs -y &>>log_file
 echo $?
 
 
-echo -e "${color} backend storage \e[0m"
-cp backend.service /etc/systemd/system/backend.service &>>backend_file
+echo -e "${color} copy backend service file  \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>log_file
 echo $?
 
 echo -e "${color} adding user to expense \e[0m"
-useradd expense &>>backend_file
+useradd expense &>>log_file
 echo $?
 
 echo -e "${color}creating expense directory \e[0m"
-mkdir /app &>>backend_file
+mkdir /app &>>log_file
 echo $?
 
 echo -e "${color}backend file frontend download\e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>backend_file
-cd /app &>>backend_file
-unzip /tmp/backend.zip &>>backend_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
+cd /app &>>log_file
+unzip /tmp/backend.zip &>>log_file
 echo $?
 
 echo -e "${color}directory storage app \e[0m"
-cd /app &>>backend_file
+cd /app &>>log_file
 echo $?
 
 echo -e "${color}npm package install \e[0m"
-npm install &>>backend_file
+npm install &>>log_file
 echo $?
 
 echo -e "${color}install mysql software \e[0m"
-dnf install mysql -y &>>backend_file
-mysql -h <mysql-dev.roshinitallam7.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>backend_file
+dnf install mysql -y &>>log_file
+mysql -h <mysql-dev.roshinitallam7.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
 echo $?
 
 echo -e "${color}reload backend softwware\e[0m"
-systemctl daemon-reload &>>backend_file
+systemctl daemon-reload &>>log_file
 echo $?
 
 echo -e "${color}enable backend softwware\e[0m"
-systemctl enable backend &>>backend_file
+systemctl enable backend &>>log_file
 echo $?
 
 echo -e "${color}start backend softwware\e[0m"
-systemctl start backend &>>backend_file
+systemctl start backend &>>log_file
 echo $?
 
 
--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,23 @@
-mysql_file=/temp/mysql.log
+log_file=/temp/expense.log
 color="\e[36m"
 
 echo -e "${color} disable mysql software \e[0m"
-dnf module disable mysql -y &>>mysql_file
+dnf module disable mysql -y &>>log_file
 echo $?
 
 echo -e "${color} copy to mysql repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo &>>mysql_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
 echo $?
 
 echo -e "${color} install mysql community server \e[0m"
-dnf install mysql-community-server -y &>>mysql_file
+dnf install mysql-community-server -y &>>log_file
 echo $?
 
 echo -e "${color} enable mysql \e[0m"
-systemctl enable mysqld &>>mysql_file
-systemctl start mysqld  &>>mysql_file
+systemctl enable mysqld &>>log_file
+systemctl start mysqld  &>>log_file
 echo $?
 
 echo -e "${color} mysql root expense project user \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>mysql_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
 echo $?
